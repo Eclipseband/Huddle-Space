35c7a272871e77835faa613080b4d5b7422ffff0
John Gregory <eclipsebandjh@gmail.com>
2026-07-24 20:55:06 -0500

    Add constants for avatar colors and reactions

--- huddle-space v2/src/constants.js
+++ huddle-space v2/src/constants.js
@@ -1,1 +1,5 @@
-
+export const AVATAR_COLORS = ["#FF8A4C", "#C98C82", "#8B8B93", "#B08968", "#6E7B6B", "#9C6644"];
+export const REACTIONS = ["❤️", "😂", "👍", "😮", "😢"];
+export const PROFILE_KEY = "huddle-space-profile";
+export const ADMIN_NAMES = ["John#6"];
+export const ADMIN_PIN = "1177";
